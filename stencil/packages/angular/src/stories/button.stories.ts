cc4bd0a72f11118a3811878eababe589793e982f
José Manuel Serrano Mármol <jserrano@plainconcepts.com>
2024-09-18 18:36:38 +0200

    Add Storybook to angular projects

---
+++ stencil/packages/angular/src/stories/button.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/angular";
+
+import { StButton } from "../index";
+
+const meta: Meta<StButton & { content: string }> = {
+  title: "Components/Button",
+  component: StButton,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    content: {
+      type: "string",
+      name: "content",
+    },
+  },
+  args: {
+    content: "My button",
+  },
+  render: (args: StButton) => ({
+    props: { ...args },
+    template: "<st-button>{{content}}</st-button>",
+  }),
+};
+
+export default meta;
+
+export const Primary: StoryObj<StButton & { content: string }> = {
+  args: {},
+};
